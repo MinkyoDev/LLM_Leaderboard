cc0f431fe9427295b3cac301dc1b0465c5640980
MinkyoJeong1 <isakin.tyrfing@gmai.com>
2024-01-30 15:46:27 +0900

    FEAT: openai api async

--- backend/domain/openai/openai_router.py
+++ backend/domain/openai/openai_router.py
@@ -11,13 +11,13 @@ router = APIRouter(
 
 @router.post("/openai", 
              description="OpenAI API를 이용한 챗봇입니다. model은 'gpt-3.5-turbo-1106', 'gpt-4', 'gpt-4-1106-preview'가 있습니다.", 
-             tags=["LLMs", "openai"])
+             tags=["LLMs"])
 async def langchain_generator(generator_schema: GeneratorSchema):
     print(f"input : {generator_schema.content}")
     print(f"model : {generator_schema.model}")
     print(f"secretKey : {generator_schema.secretKey}")
     
-    answer, tokens, execution_time = openai_api(generator_schema.content, generator_schema.model, generator_schema.secretKey)
+    answer, tokens, execution_time = await openai_api(generator_schema.content, generator_schema.model, generator_schema.secretKey)
 
     final_response = {
         "answer": {
